e5272a035763258c1345cccadddfb9a728ade1bf
David Vicent <djassiell@gmail.com>
2018-03-21 17:15:23 -0400

    first commit

---
+++ src/playlist/components/media.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+import './media.css';
+import PropTypes from 'prop-types';
+class Media extends PureComponent {
+
+	/*constructor(props)  {
+		super(props)
+		this.state = {
+			author: 'leonidas esteban'
+		}
+	}*/
+	// handleClick = (event) => {
+	// 	// console.log(this.props.image)
+	// 	this.setState({
+	// 		author: 'ricardo celis'
+	// 	})
+	// }
+	render() {
+		return (
+			<div className="Media" onClick={this.props.handleClick}>
+				<div className="Media-cover">
+					<img className="Media-image"
+						src={this.props.cover}
+						alt=""
+						width={260}
+           				height={160}
+					/>
+					<h3 className="Media-title">{this.props.title}</h3>
+					<p className="Media-author">{this.props.author}</p>
+				</div>
+			</div>
+		)
+	}
+}
+
+Media.propTypes = {
+	title: PropTypes.string.isRequired,
+	cover: PropTypes.string,
+	author: PropTypes.string,
+	type: PropTypes.oneOf(['video', 'audio'])
+}
+
+export default Media;
